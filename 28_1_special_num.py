424e5e1c8c28179f3bd378cdc560b38a885b143c
Daniel Tsakov <aristander5@gmail.com>
2026-05-19 19:44:10 +0300

    task

--- 28_1_special_num.py
+++ 28_1_special_num.py
@@ -8,7 +8,9 @@ for num in range(1, number + 1):
         sum_of_digits += digits % 10
         digits = int(digits / 10)
 
-    if (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11):
+    is_special = (sum_of_digits == 5) or (sum_of_digits == 7) or (sum_of_digits == 11)
+
+    if is_special:
             print(f"{num} -> True")
     else:
             print(f"{num} -> False")
